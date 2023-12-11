33e869839e3589d829567563789c8ddab23d5fb0
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-12-11 20:58:08 +0200

    Fixed twgl

--- webgl-demo.js
+++ webgl-demo.js
@@ -1,20 +1,11 @@
+"use strict";
+
 const canvas = document.querySelector("#glcanvas");
-var gl = canvas.getContext("webgl2");
+var gl = canvas.getContext("webgl");
 var program = webglUtils.createProgramFromScripts(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
-
-// look up where the vertex data needs to go.
-var positionLocation = gl.getAttribLocation(program, "a_position");
-var colorLocation = gl.getAttribLocation(program, "a_color");
-
-// lookup uniforms
-var matrixLocation = gl.getUniformLocation(program, "u_matrix");
-var fudgeLocation = gl.getUniformLocation(program, "u_fudgeFactor");
-
-
-const v3 = gl.v3;
-const m44 = gl.m4;
 var eyePosition;
 var target;
+
 // globals
 var pixelRatio = window.devicePixelRatio || 1;
 var scale = 1;
@@ -31,15 +22,13 @@ target = new Float32Array([23, 16, 0]);
 var up = new Float32Array([0, 1, 0]);
 var v3t0 = new Float32Array(3);
 var zeroMat = new Float32Array(16);
-
 var targetToEye = new Float32Array(3);
-v3.subtract(eyePosition, target, targetToEye);
 
+const m4 = twgl.m4;
+const v3 = twgl.v3;
 // uniforms.
 var sharedUniforms = {
 };
-
-
 
 // Create a buffer to put positions in
 var positionBuffer = gl.createBuffer();
@@ -77,56 +66,9 @@ function drawScene() {
 
   // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
-
-  // Turn on the position attribute
-  gl.enableVertexAttribArray(positionLocation);
-
-  // Bind the position buffer.
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 3;          // 3 components per iteration
-  var type = gl.FLOAT;   // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-      positionLocation, size, type, normalize, stride, offset);
-
-  // Turn on the color attribute
-  gl.enableVertexAttribArray(colorLocation);
-
-  // Bind the color buffer.
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-
-  // Tell the attribute how to get data out of colorBuffer (ARRAY_BUFFER)
-  var size = 3;                 // 3 components per iteration
-  var type = gl.UNSIGNED_BYTE;  // the data is 8bit unsigned values
-  var normalize = true;         // normalize the data (convert from 0-255 to 0-1)
-  var stride = 0;               // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;               // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-      colorLocation, size, type, normalize, stride, offset);
-
-  // Compute the matrices
-  var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
-  console.log(`matrix : ${matrix}`)
-  var translation = [45, 150, 0];
-  var rotation = [degToRad(40), degToRad(25), degToRad(325)];
-  var scale = [1, 1, 1];
-  var fudgeFactor = 1;
-  // Set the matrix.
-  gl.uniformMatrix4fv(matrixLocation, false, matrix);
-
-  // Set the fudgeFactor
-  gl.uniform1f(fudgeLocation, fudgeFactor);
-
-  // Draw the geometry.
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 16 * 6;
-  gl.drawArrays(primitiveType, offset, count);
 }
+
+
 
 function main() {
   // Initialize the GL context
@@ -138,6 +80,12 @@ function main() {
     );
     return;
   }
+  twgl.addExtensionsToContext(gl);
+  if (!gl.drawArraysInstanced || !gl.createVertexArray) {
+    alert("need drawArraysInstanced and createVertexArray"); // eslint-disable-line
+    return;
+  }
+
   const cam = new StereoCamera(2000.0, 70.0, 1.33, 45.0, 10.0, 20000.0)
   cam.ApplyLeftFrustum()
   drawScene()
@@ -158,7 +106,7 @@ class StereoCamera {
     this.mAspectRatio = AspectRatio
     this.mFOV = FOV
     this.mNearClippingDistance = NearClippingDistance
-    this.mFarClippingDistance = FarClippingDistance
+    this.mNearClippingDistance = FarClippingDistance
 
 
     this.ApplyLeftFrustum = function()
@@ -171,8 +119,8 @@ class StereoCamera {
         let c = a + this.mEyeSeparation / 2;
         left    = -b * this.mNearClippingDistance/this.mConvergence;
         right   =  c * this.mNearClippingDistance/this.mConvergence;
-        var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
-        
+        let dstopt
+        m4.frustum(left, right, bottom, top, this.mNearClippingDistance, this.mNearClippingDistance, dstopt)        
         console.log(`a : ${a} b : ${b} c : ${c}`)
 
     }
@@ -190,148 +138,6 @@ class StereoCamera {
     }
   }
 }
-
-
-var m4 = {
-
-  projection: function(width, height, depth) {
-    // Note: This matrix flips the Y axis so 0 is at the top.
-    return [
-       2 / width, 0, 0, 0,
-       0, -2 / height, 0, 0,
-       0, 0, 2 / depth, 0,
-      -1, 1, 0, 1,
-    ];
-  },
-
-  multiply: function(a, b) {
-    var a00 = a[0 * 4 + 0];
-    var a01 = a[0 * 4 + 1];
-    var a02 = a[0 * 4 + 2];
-    var a03 = a[0 * 4 + 3];
-    var a10 = a[1 * 4 + 0];
-    var a11 = a[1 * 4 + 1];
-    var a12 = a[1 * 4 + 2];
-    var a13 = a[1 * 4 + 3];
-    var a20 = a[2 * 4 + 0];
-    var a21 = a[2 * 4 + 1];
-    var a22 = a[2 * 4 + 2];
-    var a23 = a[2 * 4 + 3];
-    var a30 = a[3 * 4 + 0];
-    var a31 = a[3 * 4 + 1];
-    var a32 = a[3 * 4 + 2];
-    var a33 = a[3 * 4 + 3];
-    var b00 = b[0 * 4 + 0];
-    var b01 = b[0 * 4 + 1];
-    var b02 = b[0 * 4 + 2];
-    var b03 = b[0 * 4 + 3];
-    var b10 = b[1 * 4 + 0];
-    var b11 = b[1 * 4 + 1];
-    var b12 = b[1 * 4 + 2];
-    var b13 = b[1 * 4 + 3];
-    var b20 = b[2 * 4 + 0];
-    var b21 = b[2 * 4 + 1];
-    var b22 = b[2 * 4 + 2];
-    var b23 = b[2 * 4 + 3];
-    var b30 = b[3 * 4 + 0];
-    var b31 = b[3 * 4 + 1];
-    var b32 = b[3 * 4 + 2];
-    var b33 = b[3 * 4 + 3];
-    return [
-      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
-    ];
-  },
-
-  translation: function(tx, ty, tz) {
-    return [
-       1,  0,  0,  0,
-       0,  1,  0,  0,
-       0,  0,  1,  0,
-       tx, ty, tz, 1,
-    ];
-  },
-
-  xRotation: function(angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-
-    return [
-      1, 0, 0, 0,
-      0, c, s, 0,
-      0, -s, c, 0,
-      0, 0, 0, 1,
-    ];
-  },
-
-  yRotation: function(angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-
-    return [
-      c, 0, -s, 0,
-      0, 1, 0, 0,
-      s, 0, c, 0,
-      0, 0, 0, 1,
-    ];
-  },
-
-  zRotation: function(angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-
-    return [
-       c, s, 0, 0,
-      -s, c, 0, 0,
-       0, 0, 1, 0,
-       0, 0, 0, 1,
-    ];
-  },
-
-  scaling: function(sx, sy, sz) {
-    return [
-      sx, 0,  0,  0,
-      0, sy,  0,  0,
-      0,  0, sz,  0,
-      0,  0,  0,  1,
-    ];
-  },
-
-  translate: function(m, tx, ty, tz) {
-    return m4.multiply(m, m4.translation(tx, ty, tz));
-  },
-
-  xRotate: function(m, angleInRadians) {
-    return m4.multiply(m, m4.xRotation(angleInRadians));
-  },
-
-  yRotate: function(m, angleInRadians) {
-    return m4.multiply(m, m4.yRotation(angleInRadians));
-  },
-
-  zRotate: function(m, angleInRadians) {
-    return m4.multiply(m, m4.zRotation(angleInRadians));
-  },
-
-  scale: function(m, sx, sy, sz) {
-    return m4.multiply(m, m4.scaling(sx, sy, sz));
-  },
-
-};
 
 // Fill the buffer with the values that define a letter 'F'.
 function setGeometry(gl) {
