9498c3290d4e15ae15c7fd306b050e4f452286f2
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-12-20 19:32:24 +0200

    rewritten some logic

--- webgl-demo.js
+++ webgl-demo.js
@@ -12,50 +12,46 @@ const settings = {
   convergence: 100,
 };
 
-const gl = canvas.getContext('webgl');
-// setup GLSL program
-// compiles shader, links program, look up locations
-const textureProgramInfo = webglUtils.createProgramInfo(gl, ['vertex-shader-3d', 'fragment-shader-3d']);
+let gl = canvas.getContext('webgl');  // The WebGL
+let stereoCam // Object holding stereo camera calc params.
+let spaceball // a simple rotator object
+let surface
+let shProgram
 
-const sphereBufferInfo = primitives.createSphereBufferInfo(
-    gl,
-    1,  // radius
-    12, // subdivisions around
-    6,  // subdivisions down
-);
-const planeBufferInfo = primitives.createPlaneBufferInfo(
-    gl,
-    20,  // width
-    20,  // height
-    1,   // subdivisions across
-    1,   // subdivisions down
-);
+// Vertex shader
+var vshader = `
+attribuite vec3 vertex;
+uniform mat4 ModelViewMatrix;
+uniform mat4 ModelProjectonMatrix;
 
-// make a 8x8 checkerboard texture
-const checkerboardTexture = gl.createTexture();
-gl.bindTexture(gl.TEXTURE_2D, checkerboardTexture);
-gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,                // mip level
-    gl.LUMINANCE,     // internal format
-    8,                // width
-    8,                // height
-    0,                // border
-    gl.LUMINANCE,     // format
-    gl.UNSIGNED_BYTE, // type
-    new Uint8Array([  // data
-      0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC,
-      0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF,
-      0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC,
-      0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF,
-      0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC,
-      0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF,
-      0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC,
-      0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF, 0xCC, 0xFF,
-    ]));
-gl.generateMipmap(gl.TEXTURE_2D);
-gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+void main() {
+    gl_Position = ModelViewMatrix * ModelProjectonMatrix * vec4(vertex, 1.0);
+}`;
 
+// Fragment shader
+var fshader = `
+#ifdef GL_FRAGMENT_PRECISION_HIGH 
+    precision highp float;
+#else
+    precision mediump float;
+#endif
+
+uniform vec4 color;
+void main() {
+    gl_FragColor = color;
+}`;
+
+// Compile program
+//var program = compile(gl, vshader, fshader);
+var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vshader);
+gl.compileShader(vertexShader);
+
+var fragmentShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(fragmentShader, fshader);
+gl.compileShader(fragmentShader);
+
+/*
 webglLessonsUI.setupUI(document.querySelector('#ui'), settings, [
   { type: 'slider',   key: 'cameraX',    min: -10, max: 10, change: render, precision: 2, step: 0.001, },
   { type: 'slider',   key: 'cameraY',    min:   1, max: 20, change: render, precision: 2, step: 0.001, },
@@ -67,175 +63,191 @@ webglLessonsUI.setupUI(document.querySelector('#ui'), settings, [
   { type: 'slider',   key: 'eye_seperation',    min:   1.0, max: 200.0, change: render, precision: 2, step: 0.001, },
   { type: 'slider',   key: 'convergence',       min:   0.0, max: 5000.0, change: render, precision: 2, step: 0.001, },
 ]);
+*/
 function degToRad(d) {
   return d * Math.PI / 180;
+}
+
+function Model(name) {
+  this.name = name
+  this.iVertexBuffer = gl.createBuffer();
+  this.count = 0;
+
+  this.BufferData = function(vertices) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
+
+    this.count = vertices.length / 3;
+  }
+
+  this.Draw = function() {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+    gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iAttribVertex);
+
+    gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+  }
+}
+
+function draw() {
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
+  let translateToPointZero = m4.translation(0, 0, -10);
+
+  let modelView = spaceball.getViewMatrix();
+  /*
+  // Set the values of the projection transformation
+  let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
+
+  // Get the view from SimpleRotator
+  let modelView = spaceball.getViewMatrix();
+
+  let matAccum0 = m4.multiply(rotateToPointZero, modelView);
+  let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+
+  let modelViewProjection = m4.multiply(projection, matAccum1);
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelViewProjection);
+
+  //Draw the six faces of a cube
+  */
+  gl.uniform4fv(shProgram.iColor, [1,1,0,1]);
+
+  let matrleftfrust = stereoCam.applyLeftFrustum();
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, matrleftfrust);
+
+  let matAccum0 = m4.multiply(rotateToPointZero, modelView);
+  let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
+
+  let translateLeftEye = m4.translation(-stereoCam.eyeSeperation/2, 0, 0);
+  let modelViewLeft = m4.multiply(translateLeftEye, matAccum1);
+
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelViewLeft);
+
+  gl.colorMask(true, false, false, false);
+  surface.Draw();
+
+  gl.clear(gl.DEPTH_BUFFER_BIT);
+  gl.colorMask(false, true, true, false);
+
+  let matrightfrustum = stereoCam.applyRightFrustum();
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, matrightfrustum);
+
+  let translateRightEye = m4.translation(stereoCam.eyeSeperation / 2, 0, 0);
+  let modelViewRight = m4.multiply(translateRightEye, matAccum1);
+
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelViewRight);
 }
 
 function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
   const canvas = document.querySelector('#canvas');
-  document.getElementById("applyLeft").onclick = applyLeftFrustum;
-  document.getElementById("applyRight").onclick = applyRightFrustum;
   if (!gl) {
     return;
   }
-  render();
+  initGL();
+  draw();
 }
 
+function createSurfaceData()
+{
+  let vertexList = [];
 
-// Uniforms for each object.
-const planeUniforms = {
-  u_colorMult: [0.5, 0.5, 1, 1],  // lightblue
-  u_texture: checkerboardTexture,
-  u_world: m4.translation(0, 0, 0),
-};
-const sphereUniforms = {
-  u_colorMult: [1, 0.5, 0.5, 1],  // pink
-  u_texture: checkerboardTexture,
-  u_world: m4.translation(0, 2, 0),
-};
-
-
-function render() {
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  gl.enable(gl.CULL_FACE);
-  gl.enable(gl.DEPTH_TEST);
-
-  gl.clearColor(0.0, 0.0, 0.0, 0.0);
-  gl.clearDepth(1);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-  const cameraPosition = [settings.cameraX, settings.cameraY, settings.cameraZ];
-  const target = [0, 0, 0];
-  const up = [0, 1, 0];
-  const cameraMatrix = m4.lookAt(cameraPosition, target, up);
-  const worldMatrix = m4.translation(0, 0, 0)
-
-  const top = Math.tan(degToRad(settings.FOV)* 0.5) * settings.znear;
-  const bottom = -top;
-  const left = settings.aspect * bottom;
-  const right = settings.aspect * top;
-  const width = Math.abs(right - left);
-  const height = Math.abs(top - bottom);
-  drawScene(m4.frustum(left, right, bottom, top, settings.znear, settings.zfar), m4.inverse(cameraMatrix), worldMatrix);
+  for (let i =0; i < 360; i++) {
+    vertexList.push( Math.sin(degToRad(i), 1, Math.cos(degToRad(i))));
+    vertexList.push( Math.sin(degToRad(i), 0, Math.cos(degToRad(i))))
+  }
+}
+function StereoCamera(eyeSeperation, 
+                      convergence, 
+                      fov, 
+                      aspect, 
+                      znear, 
+                      zfar) {
+  this.eyeSeperation = eyeSeperation
+  this.convergence = convergence
+  this.znear = znear
+  this.zfar = zfar
+  this.fov = fov
+  this.aspect = aspect
 }
 
 function applyLeftFrustum() {
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(settings.eye_seperation/2, 0, gl.canvas.width, gl.canvas.height);
-
-  gl.enable(gl.CULL_FACE);
-  gl.enable(gl.DEPTH_TEST);
-
-  // Clear the canvas AND the depth buffer.
-  let red = 1.0;
-  let green = 0.0;
-  let blue = 0.0;
-  let alpha = 0.0;
-  gl.clearColor(0.0, 1.0, 1.0, 1.0);
-  gl.clearDepth(1);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-  const cameraPosition = [settings.cameraX, settings.cameraY, settings.cameraZ];
-  const target = [0, 0, 0];
-  const up = [0, 1, 0];
-  const cameraMatrix = m4.lookAt(cameraPosition, target, up);
-  const worldMatrix = m4.translation(0, 0, 0)
-
-  const top = Math.tan(degToRad(settings.FOV)* 0.5) * settings.znear;
+  const top = Math.tan(degToRad(this.fov)* 0.5) * this.znear;
   const bottom = -top;
 
-  var a = settings.aspect * Math.tan(degToRad(settings.FOV)/2) * settings.convergence;
+  var a = this.aspect * Math.tan(degToRad(this.fov)/2) * this.convergence;
   
-  var b = a - settings.eye_seperation/2;
-  var c = a + settings.eye_seperation/2;
+  var b = a - this.eyeSeperation/2;
+  var c = a + this.eyeSeperation/2;
 
-  const left = -b * settings.znear / settings.convergence;
-  const right = c * settings.znear / settings.convergence;
+  const left = -b * this.znear / this.convergence;
+  const right = c * this.znear / this.convergence;
 
-  const width = Math.abs(right - left);
-  const height = Math.abs(top - bottom);
-  drawScene(m4.frustum(left, right, bottom, top, settings.znear, settings.zfar), m4.inverse(cameraMatrix), worldMatrix);
+  return m4.frustum(left, right, bottom, top, this.znear, this.zfar);
 }
 
 function applyRightFrustum() {
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(-settings.eye_seperation/2, 0, gl.canvas.width, gl.canvas.height);
-
-  gl.enable(gl.CULL_FACE);
-  gl.enable(gl.DEPTH_TEST);
-
-  // Clear the canvas AND the depth buffer.
-  let red = 0;
-  let green = 1;
-  let blue = 1;
-  let alpha = 0;
-  gl.clearColor(1.0, 0.0, 0.0, 1.0);
-  gl.clearDepth(1);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-  const cameraPosition = [settings.cameraX, settings.cameraY, settings.cameraZ];
-  const target = [0, 0, 0];
-  const up = [0, 1, 0];
-  const cameraMatrix = m4.lookAt(cameraPosition, target, up);
-  const worldMatrix = m4.translation(0, 0, 0)
-
-  const top = Math.tan(degToRad(settings.FOV)* 0.5) * settings.znear;
+  const top = Math.tan(degToRad(this.fov)* 0.5) * this.znear;
   const bottom = -top;
 
-  var a = settings.aspect * Math.tan(degToRad(settings.FOV)/2) * settings.convergence;
+  var a = this.aspect * Math.tan(degToRad(this.fov)/2) * this.convergence;
   
-  var b = a - settings.eye_seperation/2;
-  var c = a + settings.eye_seperation/2;
+  var b = a - this.eyeSeperation/2;
+  var c = a + this.eyeSeperation/2;
 
-  const left = -c * settings.znear / settings.convergence;
-  const right = b * settings.znear / settings.convergence;
+  const left = -c * this.znear / this.convergence;
+  const right = b * this.znear / this.convergence;
 
-  drawScene(m4.frustum(left, right, bottom, top, settings.znear, settings.zfar), m4.inverse(cameraMatrix), worldMatrix);
+  return m4.frustum(left, right, bottom, top, this.znear, this.zfar);
 }
 
-function drawScene(projectionMatrix, viewMatrix, worldMatrix) {
-  // Make a view matrix from the camera matrix.
-  gl.useProgram(textureProgramInfo.program);
-
-  // Set the uniform that both the sphere and the plane share
-  webglUtils.setUniforms(textureProgramInfo, {
-    u_view: viewMatrix,
-    u_projection: projectionMatrix,
-    u_world: m4.translation(2, 0, 0),
-  });
-
+function initGL() {
   
-// ------ Draw the sphere --------
+  let prog = createProgram(gl, vshader, fshader);
+  shProgram = new ShaderProgram('Basic', prog);
+  shProgram.Use();
 
-  // Setup all the needed attributes.
-  webglUtils.setBuffersAndAttributes(gl, textureProgramInfo, sphereBufferInfo);
+  shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
+  shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
+  shProgram.iModelProjectionMatrix = gl.getUniformLocation(prog, "ModelProjectonMatrix");
+  shProgram.iColor = gl.getUniformLocation(prog, "color");
 
-  // Set the uniforms unique to the sphere
-  webglUtils.setUniforms(textureProgramInfo, sphereUniforms);
+  surface = new Model("Surface");
+  surface.BufferData(CreateSurfaceData());
 
-  // calls gl.drawArrays or gl.drawElements
-  webglUtils.drawBufferInfo(gl, sphereBufferInfo);
+  gl.enable(gl.DEPTH_TEST);
 
-  // ------ Draw the plane --------
+  stereoCam = new StereoCamera(
+    70.0,
+    5000.0,
+    degToRad(60.0),
+    1.5,
+    10.0,
+    20000.0
+  );
+}
 
-  // Setup all the needed attributes.
-  webglUtils.setBuffersAndAttributes(gl, textureProgramInfo, planeBufferInfo);
+function ShaderProgram(name, program) {
+  
+  this.name = name
+  this.prog = program
 
-  // Set the uniforms unique to the plane
-  webglUtils.setUniforms(textureProgramInfo, planeUniforms);
+  // Location of the attribute variable in the shader program
+  this.iAttribVertex = -1;
+  this.iColor = -1;
+  this.iModelViewMatrix = -1;
+  this.iProjectionMatrix = -1;
 
-  // calls gl.drawArrays or gl.drawElements
-  webglUtils.drawBufferInfo(gl, planeBufferInfo);
+  this.Use = function() {
+    gl.useProgram(this.prog);
+  }
+}
+
+function createProgram(gl, vs, fs) {
+  webglUtils.createProgramFromSources(gl, [vs, fs])
 }
 
 main();
