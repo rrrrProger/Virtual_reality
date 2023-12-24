6a42c6d75ea427f0437002a5b7c14b76c4dd12e0
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-12-24 19:40:24 +0200

    Added server.js which holds connection to the server. Added websocket connection to the sensor.

--- README.md
+++ README.md
@@ -1,26 +1,11 @@
-Lab 2 Virtual Reality.
-WebGL is used as main library. 
-Work examples:
-First initial position:
-![image-7.png](images/image-7.png)
-In the right top corner there are parameters that can be changed (FOV, cameraX, cameraY, cameraZ, aspect ratio, znear, zfar, eye_seperation - distance between the eyes, convergence)
-For example let's try to change FOV, ratio and znear and apply left and right frustum
-Let's try to change some params:
-With changed FOV:
-![image-8.png](images/image-8.png)
+Lab 3 Virtual Reality.
+Orienting in space using gyroscope.
 
-Let's add texture video:
-Function loadWebCamTexture() setups web cam texture. Because of virtual camera (OBS) on the computer attempt of capturing video from camera by navigator.mediaDevices was unsuccessful.
-
-![image-9.png](images/image-9.png)
-
-Was added interval to play video:
-
-From main():   
-    setInterval(draw, 1/20);
-
-Another frames from the video:
-
-![image-10.png](images/image-10.png)
-
-![image-11.png](images/image-11.png)
+Run server with:
+```
+node server.js
+```
+Logs are 
+```
+Server started at http://localhost:8080
+```
--- webgl-demo.js
+++ webgl-demo.js
@@ -34,7 +34,6 @@ varying vec2 v_texcoord;
 
 void main() {
    gl_Position = ModelProjectionMatrix * ModelViewMatrix * a_position;
-   v_texcoord = a_texcoord;
 }`;
 
 // Fragment shader
@@ -47,7 +46,7 @@ uniform sampler2D u_texture;
 uniform vec4 color;
 
 void main() {
-  gl_FragColor = texture2D(u_texture, v_texcoord) * color;
+  gl_FragColor = color;
 }`;
 
 // Compile program
@@ -73,7 +72,6 @@ if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
 }
 // look up where the vertex data needs to go.
 var positionLocation = gl.getAttribLocation(program, "a_position");
-var texcoordLocation = gl.getAttribLocation(program, "a_texcoord");
 
 // lookup uniforms
 
@@ -96,42 +94,6 @@ webglLessonsUI.setupUI(document.querySelector('#ui'), settings, [
   { type: 'slider',   key: 'eyeSeperation',    min:   0.01, max: 100.0, change: draw, precision: 2, step: 0.001, },
   { type: 'slider',   key: 'convergence',       min:   0.0, max: 10000.0, change: draw, precision: 2, step: 0.001, },
 ]);
-
-var copyVideo = false;
-
-function setupVideo(url) {
-  video = document.createElement("video");
-
-  let playing = false;
-  let timeupdate = false;
-
-  video.playsInline = true;
-  video.muted = true;
-  video.loop = true;
-  video.width = 200;
-  video.height = 200;
-
-  // Waiting for these 2 events ensures
-  // there is data in the video
-  video.addEventListener('playing', function() {
-      playing = true;
-      checkReady();
-  }, true);
-  video.addEventListener('timeupdate', function() {
-      timeupdate = true;
-      checkReady();
-  }, true);
-  function checkReady() {
-      if (playing && timeupdate) {
-          copyVideo = true;
-      }
-  }
-
-  video.src = url
-  video.play();
-
-  return video;
-}
 
 function StereoCamera(eyeSeperation, 
     convergence, 
@@ -202,124 +164,6 @@ function Model(name) {
   }
 }
 
-function loadWebCamTexture() {
-  var positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Put a 2 unit quad in the buffer
-  var positions = [
-    0.5, 0.5,
-    -0.5,  0.5,
-    0.5, -0.5,
-    -0.5, -0.5,
-    -0.5,  0.5,
-    0.5,  -0.5,
-  ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  // Create a buffer for texture coords
-  var texcoordBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-
-  // Put texcoords in the buffer
-  var texcoords = [
-    0, 0,
-    0, 1,
-    1, 0,
-    1, 0,
-    0, 1,
-    1, 1,
-  ];
-
-  const textureCoordinates = [
-    // Front
-    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
-    // Back
-    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
-    // Top
-    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
-    // Bottom
-    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
-    // Right
-    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
-    // Left
-    0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
-  ];
-
-  
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texcoords), gl.STATIC_DRAW);
-
-  // creates a texture info { width: w, height: h, texture: tex }
-  // The texture will start with 1x1 pixels and be updated
-  // when the image has loaded
-  function loadImageAndCreateTextureInfo() {
-    var tex = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, tex);
-    // Fill the texture with a 1x1 blue pixel.
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-                  new Uint8Array([0, 0, 255, 255]));
-
-    // let's assume all images are not a power of 2
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-    var textureInfo = {
-      width: 1,   // we don't know the size until it loads
-      height: 1,
-      texture: tex,
-    };
-
-    textureInfo.width = video.width;
-    textureInfo.height = video.height;
-
-    gl.bindTexture(gl.TEXTURE_2D, textureInfo.texture);
-    
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
-
-    return textureInfo;
-  }
-
-  var texInfo = loadImageAndCreateTextureInfo();
-
-  function render(time) {
-
-    time *= 0.001
-    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    gl.bindTexture(gl.TEXTURE_2D, texInfo.texture);
-
-    // Tell WebGL to use our shader program pair
-
-    // Setup the attributes to pull data from our buffers
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
-    gl.enableVertexAttribArray(texcoordLocation);
-    gl.vertexAttribPointer(texcoordLocation, 2, gl.FLOAT, false, 0, 0);
-
-    var matrix = m4.multiply(translatetozero, modelview);
-    // Set the matrix.
-    gl.uniformMatrix4fv(matrixLocationProjection, false, modelview);
-    gl.uniformMatrix4fv(matrixLocationView, false, matrix);
-
-    // Tell the shader to get the texture from texture unit 0
-    gl.uniform1i(textureLocation, 0);
-
-    // draw the quad (2 triangles, 6 vertices)
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-    if (copyVideo)
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, video);
-  }
-
-  render(1);
-}
-
 function draw() {
   gl.enable(gl.DEPTH_TEST);
   gl.colorMask(true, true, true, true);
@@ -334,13 +178,6 @@ function draw() {
     settings.znear, //1
     settings.zfar // 20000
   );
-  var white1PixelTexture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, white1PixelTexture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
-                new Uint8Array([255, 255, 255, 255]));
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   let matrleftfrust = stereoCam.applyLeftFrustum();
   gl.uniformMatrix4fv(shProgram.iModelProjectionMatrix, false, matrleftfrust);
 
@@ -366,15 +203,12 @@ function draw() {
 
   gl.uniformMatrix4fv(shProgram.iModelProjectionMatrix, false, matrightfrustum);
   gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, modelViewRight);
-  gl.uniform1i(textureLocation, 0);
   surface.Draw();
 
   gl.clear(gl.DEPTH_BUFFER_BIT);
   gl.clearDepth(1);
   gl.colorMask(true, true, true, true);
   gl.uniform4fv(shProgram.iColor, [1,1,1,1]);
-  loadWebCamTexture();
-
 }
 
 function main() {
@@ -384,10 +218,8 @@ function main() {
   }
 //  spaceball = new SimpleRotator(canvas, draw, 10);
   initGL();
-
-  video = setupVideo("video_example.mp4");
-  
-  setInterval(draw, 1/20);
+  console.log('Got to main')
+  draw();
 }
 
 function createSurfaceData()
